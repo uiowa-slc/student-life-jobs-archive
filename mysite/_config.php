497d386699f51d8b5a953ed245ae4e7699db4ce3
Dustin Quam <dustinquam@me.com>
2017-05-04 11:46:23 -0500

    initial commit

---
+++ mysite/_config.php
@@ -0,0 +1,23 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+//!!!!!!!Define your database name:
+$database = 'student-life-jobs';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// Set the site locale
+i18n::set_locale('en_US');
+FulltextSearchable::enable();
+// Enable nested URLs for this site (e.g. page/sub-page/)
+if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
+
+if(Director::isLive()) {
+	Director::forceSSL();
+}
