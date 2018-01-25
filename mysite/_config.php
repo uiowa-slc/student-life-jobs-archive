33e143c54be226d14c1e8db09f8c29ce19fd22df
Dustin Quam <quamsta@gmail.com>
2018-01-25 14:19:08 -0600

    updated ss + modules

--- mysite/_config.php
+++ mysite/_config.php
@@ -21,7 +21,7 @@ if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 if(Director::isLive()) {
 	Director::forceSSL();
 }
-
+Authenticator::unregister('MemberAuthenticator');
 Authenticator::set_default_authenticator('SAMLAuthenticator');
 
 SSViewer::set_theme('division-subtheme');
